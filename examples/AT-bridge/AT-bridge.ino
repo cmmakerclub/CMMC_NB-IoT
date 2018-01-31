9d4a5e9e911be34d7e227aed9be9a5bc27949604
Nat <nat@cmmc.io>
2018-02-01 02:10:06 +0700

    update serial action.

--- examples/AT-bridge/AT-bridge.ino
+++ examples/AT-bridge/AT-bridge.ino
@@ -9,19 +9,21 @@ void setup()
   Serial.begin(57600);
   nbSerial.begin(9600);
   Serial.println("CMMC AT-Bridge started.");
-  Serial.println("BC-95 is using CR only for line ending.");
+  Serial.println("BC-95 is using only CR for line ending.");
 }
 
 void loop()
 { 
   if (Serial.available()) {
     Serial.print(">> ");
+    String input = "";
     while (Serial.available() > 0)  {
       char c = Serial.read();
-      Serial.print(c);
-      nbSerial.write(c);
+      input += c;
       delay(2);
     }
+    Serial.print(input);
+    nbSerial.write(input.c_str(), input.length());
   }
 
   while (nbSerial.available() > 0)  {
