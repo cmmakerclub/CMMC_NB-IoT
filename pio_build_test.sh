ef2bb99ba82dfa2f9f469eec8915e8ef1713e519
Nat <nat@cmmc.io>
2018-02-01 12:23:48 +0700

    update 0.0.2

--- pio_build_test.sh
+++ pio_build_test.sh
@@ -1,3 +1,4 @@
 
 platformio ci --project-option="lib_ldf_mode=2" --lib="." --board=uno "examples/AT-Bridge"
+platformio ci --project-option="lib_ldf_mode=2" --lib="." --board=uno "examples/example1"
 
